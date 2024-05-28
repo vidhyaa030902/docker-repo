813e61288807b1d502328eff7efd6ee3ba2ec1ee
vidhyaa030902 <vidhyaa.s@codincity.com>
2024-05-28 15:27:42 +0530

    initial commit

---
+++ app/app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+ return 'Hello, Docker! This is my sample application.'
+if __name__ == '__main__':
+ app.run(debug=True, host='0.0.0.0')
